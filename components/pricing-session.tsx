70b9c106dbdc721f18e210af26e4a8a15b0eb043
congthang1802 <congthangmyth@gmail.com>
2024-01-18 18:05:34 +0700

    feat:pricing session

--- components/pricing-session.tsx
+++ components/pricing-session.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import CardPricing from "@/components/card-pricing";
 
 const PricingSession = () => {
-  return <div>PricingSession</div>;
+  return (
+    <section className="mt-20 flex flex-col items-center gap-4">
+      <div className="bg-[#0b1421] max-w-fit py-1 px-4 rounded-full font-medium border border-[#151d2a]">
+        PRICING
+      </div>
+      <p className="font-bold text-5xl mb-20">
+        Unlock Your Website&#39;s Potential
+      </p>
+      <div className="flex gap-8 justify-between">
+        <CardPricing />
+        <CardPricing />
+        <CardPricing />
+        <CardPricing />
+      </div>
+    </section>
+  );
 };
 
 export default PricingSession;
